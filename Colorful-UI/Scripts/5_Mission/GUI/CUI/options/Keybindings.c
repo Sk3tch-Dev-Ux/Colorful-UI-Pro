591f19586c7cad6259a3b67b53f1bf07bbfa7289
Brian Orr <brian@dayznchill.com>
2025-06-21 11:13:22 -0500

    Refactors keybinding menu layout and logic
    Moves the keybinding menu layout to the options folder for better organization.
    
    Updates texture paths to use the shared directory.
    
    Improves the UI by renaming and repositioning buttons
    and other UI elements.

--- Colorful-UI/Scripts/5_Mission/GUI/CUI/options/Keybindings.c
+++ Colorful-UI/Scripts/5_Mission/GUI/CUI/options/Keybindings.c
@@ -8,20 +8,23 @@ modded class KeybindingsMenu extends UIScriptedMenu
 	override Widget Init()
 	{
 		Input input = GetGame().GetInput();
-		layoutRoot			= GetGame().GetWorkspace().CreateWidgets("Colorful-UI/GUI/layouts/menus/settings/cui.keybinding_menu.layout", null);
+		layoutRoot			= GetGame().GetWorkspace().CreateWidgets("Colorful-UI/GUI/layouts/options/cui.keybinding_menu.layout", null);
 		
-		m_Apply				= ButtonWidget.Cast(layoutRoot.FindAnyWidget("ApplyBtn"));
-        m_ApplyBtn_Label    = TextWidget.Cast(layoutRoot.FindAnyWidget("ApplyBtn_Label"));
-		m_Back				= ButtonWidget.Cast(layoutRoot.FindAnyWidget("BackBtn"));
-		m_Undo				= ButtonWidget.Cast(layoutRoot.FindAnyWidget("UndoBtn"));
-		m_Defaults			= ButtonWidget.Cast(layoutRoot.FindAnyWidget("ResetBtn"));
-		m_HardReset			= ButtonWidget.Cast(layoutRoot.FindAnyWidget("ResetAllBtn"));
+		m_Apply     = ButtonWidget.Cast(layoutRoot.FindAnyWidget("ApplyBtn"));
+		m_Back      = ButtonWidget.Cast(layoutRoot.FindAnyWidget("BackBtn"));
+		m_Undo      = ButtonWidget.Cast(layoutRoot.FindAnyWidget("UndoBtn"));
+		m_Defaults  = ButtonWidget.Cast(layoutRoot.FindAnyWidget("ResetBtn"));
+		m_HardReset = ButtonWidget.Cast(layoutRoot.FindAnyWidget("ResetAllBtn"));
 
-        cuiElmnt.proBtn(m_Apply,"#main_menu_exit",colorScheme.PrimaryText(),colorScheme.ButtonHover(),"", this,"OnApplyPressed");
+		cuiElmnt.proBtnCB(m_Apply,     "Apply",      colorScheme.PrimaryText(), colorScheme.ButtonHover(), this, "Apply");
+		cuiElmnt.proBtnCB(m_Back,      "Back",       colorScheme.PrimaryText(), colorScheme.ButtonHover(), this, "Back");
+		cuiElmnt.proBtnCB(m_Undo,      "Undo",       colorScheme.PrimaryText(), colorScheme.ButtonHover(), this, "Reset");
+		cuiElmnt.proBtnCB(m_Defaults,  "Defaults",   colorScheme.PrimaryText(), colorScheme.ButtonHover(), this, "SetToDefaults");
+		cuiElmnt.proBtnCB(m_HardReset, "Hard Reset", colorScheme.PrimaryText(), colorScheme.ButtonHover(), this, "HardReset");
 
-		m_TopShader 			    = layoutRoot.FindAnyWidget( "TopShader" );
-		m_BottomShader 			    = layoutRoot.FindAnyWidget( "BottomShader" );
-		m_MenuDivider				= layoutRoot.FindAnyWidget( "MenuDivider" );
+		m_TopShader    = layoutRoot.FindAnyWidget( "TopShader" );
+		m_BottomShader = layoutRoot.FindAnyWidget( "BottomShader" );
+		m_MenuDivider  = layoutRoot.FindAnyWidget( "MenuDivider" );
 
 		m_TopShader.SetColor(colorScheme.TopShader());
 		m_BottomShader.SetColor(colorScheme.BottomShader());
@@ -40,109 +43,12 @@ modded class KeybindingsMenu extends UIScriptedMenu
 		g_Game.SetKeyboardHandle(this);
 		m_Tabber.RefreshTab(true);
 		
-		// ColorDisabled(m_ApplyBtn_Label);
-		// m_ApplyBtn_Label.SetFlags(WidgetFlags.IGNOREPOINTER);
-		// ColorDisabled(m_Undo);
-		// m_Undo.SetFlags(WidgetFlags.IGNOREPOINTER);
-		// ColorWhite(m_Defaults, null);
-		// m_Defaults.ClearFlags(WidgetFlags.IGNOREPOINTER);
-		
 		return layoutRoot;
 	}
 	
     static void OnApplyPressed()
     {
         // Logic for applying changes
-        Print("Apply button clicked");
+        Print("button clicked");
     }
-
-    // void OnBackPressed()
-    // {
-    //     // Logic for returning to the previous menu
-    //     Print("Back button clicked");
-    //     CloseMenu();
-    // }
-
-    // void OnUndoPressed()
-    // {
-    //     // Logic for undoing changes
-    //     Print("Undo button clicked");
-    //     UndoChanges();
-    // }
-
-    // void OnResetPressed()
-    // {
-    //     // Logic for resetting to defaults
-    //     Print("Reset button clicked");
-    //     ResetToDefaults();
-    // }
-
-    // void OnHardResetPressed()
-    // {
-    //     // Logic for performing a hard reset
-    //     Print("Hard Reset button clicked");
-    //     PerformHardReset();
-    // }
-
-	// override void ClearKeybind(int key_index)
-	// {
-	// 	ColorWhite(m_ApplyBtn_Label, null);
-	// 	m_ApplyBtn_Label.ClearFlags(WidgetFlags.IGNOREPOINTER);
-	// 	ColorWhite(m_Undo, null);
-	// 	m_Undo.ClearFlags(WidgetFlags.IGNOREPOINTER);
-	// }
-	
-	// override void ClearAlternativeKeybind(int key_index)
-	// {
-	// 	ColorWhite(m_ApplyBtn_Label, null);
-	// 	m_ApplyBtn_Label.ClearFlags(WidgetFlags.IGNOREPOINTER);
-	// 	ColorWhite(m_Undo, null);
-	// 	m_Undo.ClearFlags(WidgetFlags.IGNOREPOINTER);
-	// }
-	
-	// //Coloring functions (Until WidgetStyles are useful)
-	// override void ColorRed(Widget w)
-	// {
-	// 	SetFocus(w);
-		
-	// 	ButtonWidget button = ButtonWidget.Cast(w);
-	// 	if (m_Apply)
-	// 	{
-	// 		m_ApplyBtn_Label.SetColor(colorScheme.ButtonHover());
-	// 	}
-	// }
-	
-	// override void ColorWhite(Widget w, Widget enterW)
-	// {
-	// 	#ifdef PLATFORM_WINDOWS
-	// 	SetFocus(null);
-	// 	#endif
-		
-	// 	ButtonWidget button = ButtonWidget.Cast(w);
-	// 	if (button)
-	// 	{
-	// 		if (button.GetFlags() & WidgetFlags.DISABLED)
-	// 		{
-	// 			button.SetTextColor(ColorManager.COLOR_DISABLED_TEXT);
-	// 		}
-	// 		else
-	// 		{
-	// 			button.SetTextColor(ColorManager.COLOR_NORMAL_TEXT);
-	// 		}
-	// 	}
-	// }
-	
-	// override void ColorDisabled(Widget w)
-	// {
-	// 	#ifdef PLATFORM_WINDOWS
-	// 	SetFocus(null);
-	// 	#endif
-		
-	// 	ButtonWidget button = ButtonWidget.Cast(w);
-	// 	if (button)
-	// 	{
-	// 		// These will be reset in the next update with the new theme manager.
-	// 		button.SetTextColor(ColorManager.COLOR_DISABLED_TEXT);
-	// 	}
-	// }
 }
