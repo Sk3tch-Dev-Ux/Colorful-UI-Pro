5d01d38c28ae126007b5f917d43dd6e5cc6c4d02
Brian Orr <brian@dayznchill.com>
2026-02-06 14:39:28 -0600

    Adds custom main menu music tracks.
    Replaces the default main menu music with custom tracks.
    
    Integrates new music files and configures the game to play them.
    The music will play in a random order.
    Volume adjusted to 0.8.

--- Colorful-UI/Scripts/3_Game/Systems/Music.c
+++ Colorful-UI/Scripts/3_Game/Systems/Music.c
@@ -4,59 +4,6 @@ modded class DynamicMusicPlayerRegistry
 	{
 		m_TracksMenu = new array<ref DynamicMusicTrackData>();
 
-		RegisterTrackMenu("DZNC_Music_Menu_SoundShader", true);
+		RegisterTrackMenu("Music_Menu_SoundSet", true);
 	}
-
-	protected void RegisterTracksCredits()
-	{
-		m_TracksCredits = new array<ref DynamicMusicTrackData>();
-
-		RegisterTrackCredits("Music_Menu_subtitles_remake_SoundSet");
-	}
-
-    protected void RegisterTracksTime()
-	{
-		m_TracksTime = new array<ref DynamicMusicTrackData>();
-
-		//DAY
-		RegisterTrackTime("Music_time_day_1_SoundSet", DynamicMusicPlayerTimeOfDay.DAY);
-		RegisterTrackTime("Music_time_day_2_SoundSet", DynamicMusicPlayerTimeOfDay.DAY);
-		RegisterTrackTime("Music_time_day_3_SoundSet", DynamicMusicPlayerTimeOfDay.DAY);
-		RegisterTrackTime("Music_time_day_4_SoundSet", DynamicMusicPlayerTimeOfDay.DAY);
-		RegisterTrackTime("Music_time_day_5_SoundSet", DynamicMusicPlayerTimeOfDay.DAY);
-		RegisterTrackTime("Music_time_day_6_SoundSet", DynamicMusicPlayerTimeOfDay.DAY);
-		RegisterTrackTime("Music_time_day_7_SoundSet", DynamicMusicPlayerTimeOfDay.DAY);
-		RegisterTrackTime("Music_time_day_8_SoundSet", DynamicMusicPlayerTimeOfDay.DAY);
-		
-        //NIGHT
-		RegisterTrackTime("Music_time_night_1_SoundSet", DynamicMusicPlayerTimeOfDay.NIGHT);
-		RegisterTrackTime("Music_time_night_2_SoundSet", DynamicMusicPlayerTimeOfDay.NIGHT);
-		
-        //DAWN
-		RegisterTrackTime("Music_time_dawn_1_SoundSet", DynamicMusicPlayerTimeOfDay.DAWN);
-		RegisterTrackTime("Music_time_dawn_2_SoundSet", DynamicMusicPlayerTimeOfDay.DAWN);
-		
-        //DUSK
-		RegisterTrackTime("Music_time_dusk_1_SoundSet", DynamicMusicPlayerTimeOfDay.DUSK);
-		RegisterTrackTime("Music_time_dusk_2_SoundSet", DynamicMusicPlayerTimeOfDay.DUSK);
-		
-        //UNSORTED (This can play at any time uncomment if you want)
-		//RegisterTrackTime("Music_time_based_new_6_SoundSet", DynamicMusicPlayerTimeOfDay.ANY);
-	}
-
-    // (This is for contaminated zones but I dont really need it, you can add it if you want.)
-    // protected void RegisterTracksLocationStatic()
-	// {
-	// 	m_TracksLocationStatic = new array<ref DynamicMusicTrackData>();
-	// 	m_TracksLocationStaticPrioritized = new array<ref DynamicMusicTrackData>();
-	// }
-
-	// protected void RegisterTracksLocationDynamic()
-	// {
-	// 	m_TracksLocationDynamic = new array<ref DynamicMusicTrackData>();
-
-	// 	RegisterTrackLocationDynamic("Music_loc_contaminated_day_SoundSet", DynamicMusicLocationTypes.CONTAMINATED_ZONE, DynamicMusicPlayerTimeOfDay.DAY);
-	// 	RegisterTrackLocationDynamic("Music_loc_contaminated_night_SoundSet", DynamicMusicLocationTypes.CONTAMINATED_ZONE, DynamicMusicPlayerTimeOfDay.NIGHT);
-	// }
 }
-	
